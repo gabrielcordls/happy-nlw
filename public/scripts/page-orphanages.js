ff735b02e94890717239e0bca08a16c01f9bcc66
Gabriel Cordeiro <gabriel.cordeiro.25@gmail.com>
2021-01-29 18:59:00 -0300

    Finalizado

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,51 @@
+// tipos de dados do JavaScript:
+// string: "" 
+// Number: 01 
+// Object: {} 
+// Boolean: true or false
+// Array: []
+
+//create map
+const map = L.map('mapid').setView([-27.222633,-49.645587], 15);// ([latitude, longitude], zoom)
+
+//create an add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+function addMarker({id, name, lat, lng} = orphanages) { 
+
+    //create icon
+    const icon = L.icon({
+      iconUrl: "/images/map-marker.svg",
+      iconSize: [58, 68],
+      iconAnchor: [29, 68],
+      popupAnchor: [170, 2]
+})
+
+    //create popup overlay
+    const popup = L.popup({
+        closeButton: false,
+        className: "map-popup",
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(
+        `${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`
+        )
+        
+    //create and add marker
+    L.marker([lat, lng], { icon })
+     .addTo(map)
+     .bindPopup(popup)    
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage)
+})
